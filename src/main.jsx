9cf71299923b09e623f1410acd8ceab6dc28f532
QuinnC <59322843+Quinn135@users.noreply.github.com>
2023-01-15 16:07:43 -0600

    First push

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './index.css'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Plans from './pages/Plans';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/plans' element={<Plans />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (
+        <>
+            <h1 class="center">
+                <strong>
+                    Welcome to RMLawnCo
+                </strong>
+            </h1>
+            <p class="paragraph" style="margin: auto; text-indent: 24px;">
+                Working with us at RMLawnCo will bring you great service, and great
+                communication. Great
+                communication can leave
+                you with a great lawn and a happy face!</p>
+        </>
+    )
+}
