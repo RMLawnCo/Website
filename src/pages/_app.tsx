d210bfcf77efe2f533e07408fc6aa5e3f9edd5c1
Quinn Cooperstein <quinn.flynn.coop@gmail.com>
2023-01-16 17:07:43 -0600

    ss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import Layout from '@/components/Layout'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '@/styles/globals.css'
+import Layout from '@/components/_layout'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
