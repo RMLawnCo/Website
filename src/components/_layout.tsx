b49434ba3a0ab1c92e63a6421ed65fd311930dc2
QuinnC <59322843+Quinn135@users.noreply.github.com>
2023-05-13 22:42:33 -0500

    uu

--- src/components/_layout.tsx
+++ src/components/_layout.tsx
@@ -11,8 +11,16 @@ export default function Layout({ children }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="description"
                     content="RMLawnCo is a landcare and landscaping comany. We provide amazing lawnmowing and landcare to landscapes in the Fairview Heights, Illinois area, all of this at an affordable price!" />
+                <meta name="og:description"
+                    content="RMLawnCo is a landcare and landscaping comany. We provide amazing lawnmowing and landcare to landscapes in the Fairview Heights, Illinois area, all of this at an affordable price!" />
                 <meta name="keywords"
                     content="RMLawnCo" />
+                <meta property="og:title" content="RMLawnCo" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://rmlawnco.com/" />
+                <meta property="og:image" content="http://rmlawnco.com/grass.jpg" />
+                <meta name="theme-color" content="#191a1b" />
+                <meta name="twitter:card" content="summary_large_image" />
                 <link rel="shortcut icon" href="/favicon.ico?v=2" />
                 <link rel="canonical" href="https://rmlawnco.com/" />
                 <title>RMLawnCo</title>
