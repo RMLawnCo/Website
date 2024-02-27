c103e6ede5be05db8eb6ce0ba8cc9fcec34bb374
QuinnC <cooperstein.quinn@gmail.com>
2024-02-27 16:30:45 -0600

    rid of phone num

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,18 @@
 export default function Contact() {
-    return (
-        <div>
-            <h1 className="centerVertical">
-                <strong>
-                    Contact us:
-                </strong>
-            </h1>
-            <p className="centerVertical">
-                <span>Phone: <a href="tel:6186800325">(618) 680 0325</a></span>
-                <span>Email: <a href="mailto:rmlawncontact@gmail.com?subject=Title&body=Message">rmlawncontact@gmail.com</a></span>
-            </p>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="centerVertical">
+        <strong>Contact us:</strong>
+      </h1>
+      <p className="centerVertical">
+        {/* <span>Phone: <a href="tel:6186800325">(618) 680 0325</a></span> */}
+        <span>
+          Email:{" "}
+          <a href="mailto:rmlawncontact@gmail.com?subject=Title&body=Message">
+            rmlawncontact@gmail.com
+          </a>
+        </span>
+      </p>
+    </div>
+  );
 }
